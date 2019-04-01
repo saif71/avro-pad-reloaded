aaa011b42a828774cb26f8118302e6589c4d9670
saif71 <epbd.saif@gmail.com>
2019-04-01 15:42:31 +0600

    stored

--- src/js/misc.js
+++ src/js/misc.js
@@ -11,10 +11,34 @@ function changeCSS(cssFile, cssLinkIndex) {
 }
 
 function switchMode() {
+
     if(document.getElementById("myCheckbox").checked === true){
-    changeCSS('/css/main_dark.css', 1);
+    changeCSS('/css/main.css', 1);
 
     } else {
-    changeCSS('/css/main.css', 1);
+    changeCSS('/css/main_dark.css', 1);
     }
+    save();
   }
+
+  function save(){
+    var checkbox = document.getElementById('myCheckbox');
+    localStorage.setItem('myCheckbox', checkbox.checked);
+    //alert('saved '+localStorage.getItem('myCheckbox'));
+    document.getElementById('modePref').innerHTML = localStorage.getItem('myCheckbox');
+
+  }
+  function loadModePref(){    
+    var checked = JSON.parse(localStorage.getItem('myCheckbox'));
+    document.getElementById("myCheckbox").checked = checked;
+    if(localStorage.getItem('myCheckbox')===true){
+        
+        changeCSS('/css/main.css', 1);
+    }
+    else{
+       
+        changeCSS('/css/main_dark.css', 1);
+    }
+}
+
+loadModePref();
