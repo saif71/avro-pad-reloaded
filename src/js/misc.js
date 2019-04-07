673dd0306ef44870c42554747c4f7a07c52bd222
saif71 <epbd.saif@gmail.com>
2019-04-08 00:04:22 +0600

    switching via jquery [Working]

--- src/js/misc.js
+++ src/js/misc.js
@@ -1,27 +1,45 @@
-function changeCSS(cssFile, cssLinkIndex) {
+// function changeCSS(cssFile, cssLinkIndex) {
 
-    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+//     var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
 
-    var newlink = document.createElement("link");
-    newlink.setAttribute("rel", "stylesheet");
-    newlink.setAttribute("type", "text/css");
-    newlink.setAttribute("href", cssFile);
+//     var newlink = document.createElement("link");
+//     newlink.setAttribute("rel", "stylesheet");
+//     newlink.setAttribute("type", "text/css");
+//     newlink.setAttribute("href", cssFile);
 
-    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+//     document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+// }
+function applyDark(){
+    $("#wrapper").addClass("wrapper_dark");
+    $("#wrapper").removeClass("wrapper_light");
+
+    $("textarea").addClass("text_dark_mode");
+    $("footer").addClass("footer_dark");
+}
+
+function applyLight(){
+    $("#wrapper").addClass("wrapper_light");
+    $("#wrapper").removeClass("wrapper_dark");
+    $("textarea").removeClass("text_dark_mode");
+    $("footer").removeClass("footer_dark");
 }
 
 function switchMode() {
 
     if (document.getElementById("myCheckbox").checked === true) {
-        changeCSS('/css/main_dark.css', 2);
+        // changeCSS('/css/main_dark.css', 2);
+        applyDark()
         localStorage.setItem("modePrefLS", "dark");
 
     } else {
-        changeCSS('/css/main.css', 2);
+        // changeCSS('/css/main.css', 2);
+        applyLight()
         localStorage.setItem("modePrefLS", "light");
     }
 
 }
+
+
 function loadModePref() {
 
     console.log(localStorage.getItem("modePrefLS")+ " mode loaded");
@@ -29,14 +47,21 @@ function loadModePref() {
 
     if (modeLoad === "light" || modeLoad === null) {
 
-        changeCSS('/css/main.css', 2);
+        applyLight()
         document.getElementById("myCheckbox").checked = false;
     }
     else {
 
-        changeCSS('/css/main_dark.css', 2);
+        applyDark()
         document.getElementById("myCheckbox").checked = true;
     }
 }
 
 loadModePref();
+
+// $(document).ready(function(){
+//     $("#myCheckbox").click(function(){
+//       $("#wrapper").toggleClass("wrapper_dark");
+//       $("footer").toggleClass("footer_dark");
+//     });
+//   });
